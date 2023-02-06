4eccfc35fcdba55c4db0803fd2291ac82cb831f2
kishor shrestha <kisshor916@gmail.com>
2023-02-06 20:57:03 +1100

    added form validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request) {
         $incomingfields=$request->validate([
-            'username'=>'required',
-            'email'=>'required',
-            'password'=>'required'
+            'username'=>['required','min:3','max:15',Rule::unique('Users','username')],
+            'email'=>['required','email',Rule::unique('Users','email')],
+            'password'=>['required','min:8','confirmed']
         ]);
+        $incomingfields['password']= bcrypt($incomingfields['password']);
         User::create($incomingfields);
         return view('register');
     }
